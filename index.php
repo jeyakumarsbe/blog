71ae6ed043e0be613262a8f3d53d982c3b2d56a4
Jayakumar <jeyakumarsbe@gmail.com>
2019-10-15 07:30:54 +0530

    First Commit

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
  * @var bool
  */
 define( 'WP_USE_THEMES', true );
-die("HI JAYAKUMAR YOU ARE GOOD AT");
+die("DEPLOY YOUR CODE BY GIT JENKINS");
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
